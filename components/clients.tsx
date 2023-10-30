aaa7c17a7ee5f1714089534e96943eeeb13c02aa
grexecution <greg.wallner@gmail.com>
2023-10-30 19:28:32 +0100

    Logo addition

--- components/clients.tsx
+++ components/clients.tsx
@@ -4,6 +4,8 @@ import UmdaschLogo from '@/public/images/logos/umdasch.svg'
 import SparLogo from '@/public/images/logos/spar.png'
 import RedbullLogo from '@/public/images/logos/redbull.svg'
 import OlivadisLogo from '@/public/images/logos/olivadis.svg'
+import HTLLogo from '@/public/images/logos/htlmoedling.svg'
+import KasLogo from '@/public/images/logos/kas.svg'
 
 import Image from "next/image";
 import React from "react";
@@ -38,12 +40,22 @@ export default function Clients() {
 
             {/* Item */}
             <div className="flex items-center justify-center h-24 border border-gray-200 dark:border-gray-800 p-2" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-clients]">
-              <Image src={OlivadisLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="133" height="30" alt="Corona Icon" />
+              <Image src={OlivadisLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="123" height="30" alt="Corona Icon" />
             </div>
 
             {/* Item */}
             <div className="flex items-center justify-center h-24 border border-gray-200 dark:border-gray-800 p-2" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-clients]">
-              <Image src={RedbullLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="113" height="30" alt="Corona Icon" />
+              <Image src={RedbullLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="100" height="30" alt="Corona Icon" />
+            </div>
+
+            {/* Item */}
+            <div className="flex items-center justify-center h-24 border border-gray-200 dark:border-gray-800 p-2" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-clients]">
+              <Image src={HTLLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="163" height="40" alt="Corona Icon" />
+            </div>
+
+            {/* Item */}
+            <div className="flex items-center justify-center h-24 border border-gray-200 dark:border-gray-800 p-2" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-clients]">
+              <Image src={KasLogo} className="max-w-full fill-current text-gray-400 dark:text-gray-500" width="110" height="60" alt="Corona Icon" />
             </div>
 
           </div>
