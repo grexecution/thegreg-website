99a7073f52d343d8fb06ced5f4b3562380ad0dbf
grexecution <greg.wallner@gmail.com>
2023-10-31 11:52:17 +0100

    Contact Button modal adaption

--- components/contactModal.tsx
+++ components/contactModal.tsx
@@ -66,7 +66,7 @@ export default function MyModal() {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <div className="flex gap-4 items-center">
+                                    <div className="flex gap-5 items-center">
                                         <div className="relative flex flex-col w-3/12 items-center">
                                             <svg className="absolute top-0 left-16 mt-1 -mr-8" width="27" height="12" xmlns="http://www.w3.org/2000/svg">
                                                 <path className="fill-current text-teal-500" d="M2.785 5.334C2.538 5.5-.2 2.944.011 2.646.826 1.483 2.183.836 3.62.5 5.064.158 6.582.117 7.92-.02c.017-.002.098.153.088.166-1.763 2.018-3.223 3.836-5.221 5.188zm3.676 6.519c-.862.184-1.937-3.403-1.07-3.711 3.422-1.22 7.078-1.671 10.728-1.766 3.655-.096 7.304.162 10.866.32.044.002.06.177.018.187-6.938 1.634-13.691 3.504-20.542 4.97z" />
@@ -77,12 +77,12 @@ export default function MyModal() {
                                             </p>
                                         </div>
                                         <div className="w-9/12">
-                                            <h2 className="h2 text-[20px] lg:text-2xl mb-0 font-red-hat-display font-black text-gray-900">
-                                                Wie möchtest du kontaktiert werden?
+                                            <h2 className="h2 text-[23px] lg:text-2xl mb-0 font-red-hat-display font-black text-gray-900">
+                                                Wie möchtest du mich kontaktieren?
                                             </h2>
                                             <div className="mt-0">
                                                 <p className="text-sm text-gray-500">
-                                                    Ich melde mich innerhalb von 24 Stunden!
+                                                    Antwortzeit: ~ 6-12h
                                                 </p>
                                             </div>
                                         </div>
