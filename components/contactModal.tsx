3291d61e38465a413a3ca8b2d63ee8ce8d5faad0
grexecution <greg.wallner@gmail.com>
2023-10-31 11:42:57 +0100

    Contact Button modal adaption

--- components/contactModal.tsx
+++ components/contactModal.tsx
@@ -51,7 +51,7 @@ export default function MyModal() {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <div className="fixed inset-0 bg-black/75" />
+                        <div className="fixed inset-0 bg-black/90" />
                     </Transition.Child>
 
                     <div className="fixed inset-0 overflow-y-auto">
@@ -68,13 +68,16 @@ export default function MyModal() {
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                                     <div className="flex gap-4 items-center">
                                         <div className="relative flex flex-col w-3/12 items-center">
+                                            <svg className="absolute top-0 left-16 mt-1 -mr-8" width="27" height="12" xmlns="http://www.w3.org/2000/svg">
+                                                <path className="fill-current text-teal-500" d="M2.785 5.334C2.538 5.5-.2 2.944.011 2.646.826 1.483 2.183.836 3.62.5 5.064.158 6.582.117 7.92-.02c.017-.002.098.153.088.166-1.763 2.018-3.223 3.836-5.221 5.188zm3.676 6.519c-.862.184-1.937-3.403-1.07-3.711 3.422-1.22 7.078-1.671 10.728-1.766 3.655-.096 7.304.162 10.866.32.044.002.06.177.018.187-6.938 1.634-13.691 3.504-20.542 4.97z" />
+                                            </svg>
                                             <Image className="rounded-full" src={TestimonialImage01} width={80} height={80} alt="Testimonial 01" />
                                             <p className="text-xs text-gray-500 text-center">
                                                 Gregor W.
                                             </p>
                                         </div>
                                         <div className="w-9/12">
-                                            <h2 className="h2 text-[22px] lg:text-2xl mb-0 font-red-hat-display font-black text-gray-900">
+                                            <h2 className="h2 text-[20px] lg:text-2xl mb-0 font-red-hat-display font-black text-gray-900">
                                                 Wie möchtest du kontaktiert werden?
                                             </h2>
                                             <div className="mt-0">
@@ -86,17 +89,17 @@ export default function MyModal() {
                                     </div>
 
 
-                                    <div className="mt-4 flex flex-col gap-4">
+                                    <div className="mt-4 flex flex-col gap-2">
                                         <button
                                             type="button"
                                             onClick={function(event){ closeModal(); contactWhatsapp()}}
-                                            className="btn w-full text-white bg-teal-500 hover:bg-teal-400 shrink-0 gap-2">
+                                            className="btn w-full text-white bg-black hover:bg-gray-900 shrink-0 gap-2 border-2">
                                             <Image src={WhatsappIcon} width={20} height={20} alt="Whatsapp Icon" />
                                             Auf Whatsapp schreiben
                                         </button>
                                         <button
                                             type="button"
-                                            className="btn text-teal-500 bg-white hover:bg-teal-100 shrink-0 border-teal-500 border-2"
+                                            className="btn text-black bg-white hover:bg-gray-100 shrink-0 border-black border-2 border-inset"
                                             onClick={function(event){ closeModal(); contactMail()}}
                                         >
                                             Email schreiben
