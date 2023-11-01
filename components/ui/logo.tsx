c782ae0d09972687fdd3e0e0b796b53bdc325450
grexecution <greg.wallner@gmail.com>
2023-11-01 11:34:27 +0100

    Header Logo Animation

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,12 +1,18 @@
 export default function Logo() {
   return (
-      <svg width="33" height="34" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="36" height="36" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M16.7553 33.3647C7.81971 33.3647 0.549805 26.0948 0.549805 17.1592C0.549805 8.22674 7.81971 0.959961 16.7553 0.959961C23.7761 0.959961 29.9314 5.45266 32.1233 12.0283H25.6037C23.773 8.865 20.4323 6.92221 16.7553 6.92221C11.1075 6.92221 6.51206 11.5145 6.51206 17.1592C6.51206 22.807 11.1075 27.4025 16.7553 27.4025C20.0369 27.4025 23.1441 25.8084 25.0651 23.1433L26.1299 21.6644H18.8289V16.4992H32.5498C32.5498 16.5583 32.5467 16.7202 32.5467 16.7202C32.5405 17.8224 32.5062 19.2608 32.3132 20.6432C32.1451 21.845 31.8617 22.9129 31.4445 23.9061C30.2583 26.7268 28.0415 29.2394 25.2021 30.9829C22.6677 32.5397 19.7442 33.3647 16.7553 33.3647Z" fill="url(#paint0_linear_119_127)"/>
           <defs>
               <linearGradient id="paint0_linear_119_127" x1="23.3656" y1="0.829664" x2="8.8269" y2="36.5948" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#809CD0"/>
-                  <stop offset="0.5026" stopColor="#3ABAB4"/>
-                  <stop offset="1" stopColor="#16716F"/>
+                  <stop stopColor="#809CD0">
+                      <animate attributeName="stop-color" values="#6FA3E7; #3ABAB4; #16716F; #6FA3E7;" dur="4s" repeatCount="indefinite"></animate>
+                  </stop>
+                  <stop offset="0.5" stopColor="#3ABAB4">
+                      <animate attributeName="stop-color" values="#3ABAB4; #16716F; #6FA3E7; #3ABAB4;" dur="4s" repeatCount="indefinite"></animate>
+                  </stop>
+                  <stop offset="1" stopColor="#16716F">
+                      <animate attributeName="stop-color" values=" #16716F; #6FA3E7; #3ABAB4; #16716F;" dur="4s" repeatCount="indefinite"></animate>
+                  </stop>
               </linearGradient>
           </defs>
       </svg>
