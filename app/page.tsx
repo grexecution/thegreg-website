1f5175e7ac204b32277f8092a0bc2ffc543e1258
grexecution <greg.wallner@gmail.com>
2023-11-02 11:17:27 +0100

    OG addition & Sitemap fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export const metadata = {
   description: 'Warum sich zwischen Funktionalität und Flair entscheiden? Ich biete Websites, die nicht nur hübsch anzusehen sind, sondern auch die Kasse klingeln lassen. Schau vorbei und bringe dein Online-Geschäft auf die Überholspur!',
 }
 
+import Head from "next/head";
 import PageIllustration from '@/components/page-illustration'
 import Hero from '@/components/hero-home'
 import PricingTabs from "@/components/pricingtabs";
@@ -17,6 +18,25 @@ import Testimonials from "@/components/testimonials";
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="description" content="Warum sich zwischen Funktionalität und Flair entscheiden? Ich biete Websites, die nicht nur hübsch anzusehen sind, sondern auch die Kasse klingeln lassen. Schau vorbei und bringe dein Online-Geschäft auf die Überholspur!" key="desc" />
+        <meta property="og:title" content="derGreg.com – Websites, die verkaufen, überzeugen und verzaubern!" />
+        <meta property="og:description" content="Warum sich zwischen Funktionalität und Flair entscheiden? Ich biete Websites, die nicht nur hübsch anzusehen sind, sondern auch die Kasse klingeln lassen. Schau vorbei und bringe dein Online-Geschäft auf die Überholspur!" />
+        <meta property="og:type" content="Website" />
+        <meta property="og:url" content="www.dergreg.com" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta property="og:image" content='@/public/images/OG/og_facebook.png' />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta property="og:image" content='@/public/images/OG/og_whatsapp.png'/>
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+
+      </Head>
       {/*  Page illustration */}
       <div className="relative max-w-6xl mx-auto h-0 pointer-events-none -z-1" aria-hidden="true">
         <PageIllustration />
